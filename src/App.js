1d84739bb8504f4a294ec861d2e0489542317561
Waynexia888 <qiwuwaynexia@gmail.com>
2020-03-29 16:27:17 -0700

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.handleInputValue = this.handleInputValue.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.state = {
+      inputValue: "hello world",
+      list: []
+    }
+  }
+
+  handleInputValue(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  handleKeyUp(e) {
+    if (e.keyCode === 13 && e.target.value !== '') {
+      const newList = [...this.state.list, this.state.inputValue];
+      this.setState({
+        list: newList,
+        inputValue: ''
+      })
+    }
+  }
+
+  handleClickValue(index) {
+    const list = [...this.state.list];
+    list.splice(index, 1)
+    this.setState({
+      list: list
+    })
+  }
+
+  getItemList() {
+    return this.state.list.map((value, index) => {
+      return (
+        <li key={index}
+          onClick={this.handleClickValue.bind(this, index)}
+          dangerouslySetInnerHTML={{__html: value}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </li>
+      )
+    })
+
+  }
+
+
+  render() {
+    return (
+      <Fragment>
+        <label htmlFor="myinput">请输入内容：</label>
+        <input id="myinput"
+          className="input"
+          value={this.state.inputValue}
+          onChange={this.handleInputValue}
+          onKeyUp={this.handleKeyUp}
+        />
+        <ul>
+          {this.getItemList()}
+        </ul>
+
+      </Fragment>
+    )
+  }
 }
 
-export default App;
+
+export default TodoList;
